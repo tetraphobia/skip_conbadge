a8f28491f5b14b5d0d3235b83789525088bce080
Jacob Teddy <tetraphobia@wyvrn.dev>
2026-04-03 16:59:43 -0400

    feat: socials view

--- __init__.py
+++ __init__.py
@@ -22,13 +22,40 @@ _last_frame_ms = 0
 CX = screen.width / 2
 CY = screen.height / 2
 
+_bg_color = color.rgb(30, 180, 255)
+
 # Views
-current_view = ARTIST_VIEW
+current_view = BADGE_VIEW
+
 views = {
     machine.Pin.board.BUTTON_A: BADGE_VIEW,
     machine.Pin.board.BUTTON_B: SOCIALS_VIEW,
     machine.Pin.board.BUTTON_C: ARTIST_VIEW,
 }
+
+# Socials
+socials = [
+    {
+        'social': 'discord',
+        'handle': 'skipkin'
+    },
+    {
+        'social': 'github',
+        'handle': 'tetraphobia'
+    },
+    {
+        'social': 'twitch',
+        'handle': 'skippykin'
+    },
+    {
+        'social': 'steam',
+        'handle': 'notvalkyrie'
+    },
+    {
+        'social': 'telegram',
+        'handle': 'skipkin'
+    },
+]
 
 def shadow_text(text, x, y, font, shadow_color=color.rgb(0, 10, 0, 90), text_color=color.rgb(255, 255, 255)):
     screen.font = font
@@ -39,12 +66,13 @@ def shadow_text(text, x, y, font, shadow_color=color.rgb(0, 10, 0, 90), text_col
     screen.pen = text_color
     screen.text(text, x, y)
 
-def draw_bg_swirl(r, g, b, alpha=50, arms = 10, speed = 1.5, curve = 0.3, bg_color=color.rgb(30, 180, 255)):
+def draw_bg_swirl(r, g, b, alpha=50, arms = 10, speed = 1.5, curve = 0.3):
+    global _bg_color
     cy = CY - 8
     cx = CX
 
-    if bg_color is not None:
-        screen.pen = bg_color
+    if _bg_color is not None:
+        screen.pen = _bg_color
         screen.rectangle(0, 0, screen.width, screen.height)
         
 
@@ -70,7 +98,10 @@ def draw_bg_swirl(r, g, b, alpha=50, arms = 10, speed = 1.5, curve = 0.3, bg_col
         y += 10
 
 def draw_badge_view():
-    global _frame_index, _last_frame_ms
+    global _frame_index, _last_frame_ms, _bg_color
+
+    _bg_color = color.rgb(222, 100, 17)
+
     draw_bg_swirl(
         r=255,
         g=245,
@@ -79,8 +110,8 @@ def draw_badge_view():
         arms=4,
         speed=3,
         curve=0.08,
-        bg_color=color.rgb(222, 100, 17)
     )
+
 
     NUM_FRAMES = 8
     SPEED = 6 # frames per second
@@ -94,8 +125,29 @@ def draw_badge_view():
         _frame_index = (_frame_index + 1) % NUM_FRAMES
         _last_frame_ms = now
 
+    # circles
+    screen.pen = color.rgb(245, 209, 184, 80)
+    screen.shape(shape.circle(45, 45, 40))
+    screen.shape(shape.circle(screen.width - 65, screen.height - 65, 60))
+
+
+    # platform
+    screen.pen = color.rgb(245, 209, 184)
+    screen.shape(shape.rectangle(screen.width / 4, screen.height - 25, screen.width / 2, 20))
+    # left edge
+    screen.triangle(
+            screen.width / 4, screen.height - 5,
+            screen.width / 4, screen.height - 26,
+            screen.width / 4 - 20, screen.height - 5,
+        )
+    screen.triangle(
+            screen.width / 4 * 3 - 1, screen.height - 5,
+            screen.width / 4 * 3, screen.height - 26,
+            screen.width / 4 * 3 + 20, screen.height - 5,
+        )
+
     # sprite
-    screen.blit(anim.frame(_frame_index), vec2(CX - sheet.sw / 2, screen.height - sheet.sh + 4))
+    screen.blit(anim.frame(_frame_index), vec2(CX - sheet.sw / 2, screen.height - sheet.sh - 5))
 
     # skip
     screen.font = rom_font.outflank
@@ -118,22 +170,35 @@ def draw_badge_view():
     screen.text("Gryphon", screen.width - 51, 29)
 
 def draw_socials_view():
+    global _bg_color
+    _bg_color=color.rgb(9, 101, 230)
+
     draw_bg_swirl(
         r=247,
         g=202,
         b=148,
-        alpha=100,
-        arms=5,
-        speed=3,
-        curve=0.08,
-        bg_color=color.rgb(232, 110, 27)
+        alpha=70,
+        arms=30,
+        speed=2,
+        curve=0.002,
     )
 
     screen.pen = color.rgb(255, 255, 255)
-    screen.text("Socials view", 10, 50)
+
+
+    vert_offset = (screen.height / 2) - (len(socials) * 20 / 2)
+
+    for s in socials:
+        icon = image.load(f"assets/socials/{s['social']}.png")
+        screen.blit(icon, vec2(5, vert_offset))
+        shadow_text(s['handle'], 25, vert_offset, font=rom_font.kobold)
+        vert_offset += 20
 
 
 def draw_artist_view():
+    global _bg_color
+    _bg_color=color.rgb(107, 24, 168)
+
     draw_bg_swirl(
         r=226,
         g=207,
@@ -142,7 +207,6 @@ def draw_artist_view():
         arms=-2,
         speed=3,
         curve=0.08,
-        bg_color=color.rgb(107, 24, 168)
     )
     # bg
     screen.pen = color.rgb(44, 26, 50, 97)
@@ -167,17 +231,12 @@ def draw_artist_view():
     # 'scan me'
     shadow_text("Scan me!", screen.width - 55, 62, font=rom_font.sins)
 
-
-
-
-
-
 def update():
     global current_view
 
-
     if (len(badge.pressed()) > 0):
-        current_view = views[badge.pressed()[-1]]
+        if badge.pressed()[-1] in views.keys():
+            current_view = views[badge.pressed()[-1]]
 
     if current_view == BADGE_VIEW:
         draw_badge_view()
